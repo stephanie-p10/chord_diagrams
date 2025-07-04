1dc489947e79233e02e9e158c49ad5c83a532373
Stephanie <sgpenner@uwaterloo.ca>
2025-07-04 10:03:47 -0400

    row col sep strategy draft

--- strategies/obstruction_inferral.py
+++ strategies/obstruction_inferral.py
@@ -2,5 +2,91 @@ import sys
 from pathlib import Path
 sys.path.append(str(Path(__file__).resolve().parent.parent))
 
+from typing import Dict, Iterable, Iterator, List, Optional, Sequence, Tuple, cast
+
+from comb_spec_searcher import DisjointUnionStrategy, StrategyFactory
+from comb_spec_searcher.exception import StrategyDoesNotApply
+
 from chords import GriddedChord
 from tiling import Tiling
+
+class ObstructionInferralStrategy(DisjointUnionStrategy[Tiling, GriddedChord]):
+    def __init__(self, gcs: Iterable[GriddedChord]):
+        self.gcs = tuple(sorted(gcs))
+        super().__init__(
+            ignore_parent=True, inferrable=True, possibly_empty=False, workable=True
+        )
+
+    def decomposition_function(self, comb_class: Tiling) -> Tuple[Tiling]:
+        return (comb_class.add_obstructions(self.gcs),)
+
+    def formal_step(self) -> str:
+        """Return a string describing the operation performed."""
+        if all(gc.is_point() for gc in self.gcs):
+            empty_cells_str = ", ".join(map(str, (gc.pos[0] for gc in self.gcs)))
+            return f"the cells {{{empty_cells_str}}} are empty"
+        return f"added the obstructions {{{', '.join(map(str, self.gcs))}}}"
+
+    def extra_parameters(
+        self, comb_class: Tiling, children: Optional[Tuple[Tiling, ...]] = None
+    ) -> Tuple[Dict[str, str], ...]:
+        if not comb_class.extra_parameters:
+            return super().extra_parameters(comb_class, children)
+        if children is None:
+            children = self.decomposition_function(comb_class)
+            if children is None:
+                raise StrategyDoesNotApply("Strategy does not apply")
+        child = children[0]
+        params: Dict[str, str] = {}
+        for assumption in comb_class.assumptions:
+            mapped_assumption = (assumption).avoiding( # fix with child.forward_map.map_assumption when implemented
+                child.obstructions
+            )
+            if mapped_assumption.gcs:
+                parent_var = comb_class.get_assumption_parameter(assumption)
+                child_var = child.get_assumption_parameter(mapped_assumption)
+                params[parent_var] = child_var
+        return (params,)
+
+    def backward_map(
+        self,
+        comb_class: Tiling,
+        objs: Tuple[Optional[GriddedChord], ...],
+        children: Optional[Tuple[Tiling, ...]] = None,
+    ) -> Iterator[GriddedChord]:
+        if children is None:
+            children = self.decomposition_function(comb_class)
+        yield children[0] # add .backward_map.map_gc(cast(GriddedChord, objs[0])) when implemented
+
+    def forward_map(
+        self,
+        comb_class: Tiling,
+        obj: GriddedChord,
+        children: Optional[Tuple[Tiling, ...]] = None,
+    ) -> Tuple[GriddedChord]:
+        if children is None:
+            children = self.decomposition_function(comb_class)
+        return (children[0],) # add .forward_map.map_gc(obj) when implemented
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}(gcs={self.gcs})"
+
+    def __str__(self) -> str:
+        return self.formal_step()
+
+    # JSON methods
+    def to_jsonable(self) -> dict:
+        """Return a dictionary form of the strategy."""
+        d: dict = super().to_jsonable()
+        d.pop("ignore_parent")
+        d.pop("inferrable")
+        d.pop("possibly_empty")
+        d.pop("workable")
+        d["gcs"] = [gc.to_jsonable() for gc in self.gcs]
+        return d
+
+    @classmethod
+    def from_dict(cls, d: dict) -> "ObstructionInferralStrategy":
+        gcs = [GriddedChord.from_dict(gc) for gc in d.pop("gcs")]
+        assert not d
+        return cls(gcs=gcs)
--- strategies/row_col_separation.py
+++ strategies/row_col_separation.py
@@ -2,6 +2,138 @@ import sys
 from pathlib import Path
 sys.path.append(str(Path(__file__).resolve().parent.parent))
 
+"""
+The row and column separation strategy. The details of the algorithm can be
+found in the algorithms folder.
+"""
+
 from chords import GriddedChord
 from tiling import Tiling
+from algorithms.row_col_sep import RowColSeparation
 
+from typing import Dict, Iterator, Optional, Tuple
+from comb_spec_searcher import DisjointUnionStrategy
+from comb_spec_searcher.exception import StrategyDoesNotApply
+
+
+__all__ = ["RowColumnSeparationStrategy"]
+
+
+Cell = Tuple[int, int]
+CellMap = Dict[Cell, Cell]
+
+
+class RowColumnSeparationStrategy(DisjointUnionStrategy[Tiling, GriddedChord]):
+    """
+    An inferral strategy that tries to separate cells in rows and columns.
+    """
+
+    _cell_maps: Dict[Tiling, Tuple[CellMap, CellMap]] = {}
+
+    def __init__(self):
+        super().__init__(
+            ignore_parent=True, inferrable=True, possibly_empty=False, workable=True
+        )
+
+    def forward_cell_map(self, tiling: Tiling) -> CellMap:
+        return self._get_cell_maps(tiling)[0]
+
+    def backward_cell_map(self, tiling: Tiling) -> CellMap:
+        return self._get_cell_maps(tiling)[1]
+
+    def _get_cell_maps(self, tiling: Tiling) -> Tuple[CellMap, CellMap]:
+        res = self._cell_maps.get(tiling)
+        if res is None:
+            forward_cell_map = self.row_col_sep_algorithm(tiling).get_cell_map()
+            backward_cell_map = {y: x for x, y in forward_cell_map.items()}
+            self._cell_maps[tiling] = forward_cell_map, backward_cell_map
+        else:
+            forward_cell_map, backward_cell_map = res
+        return forward_cell_map, backward_cell_map
+
+    def decomposition_function(
+        self, comb_class: Tiling
+    ) -> Optional[Tuple[Tiling, ...]]:
+        """Return the separated tiling if it separates, otherwise None."""
+        rcs = self.row_col_sep_algorithm(comb_class)
+        if rcs.separable():
+            return (rcs.separated_tiling(),)
+        return None
+
+    def extra_parameters(
+        self, comb_class: Tiling, children: Optional[Tuple[Tiling, ...]] = None
+    ) -> Tuple[Dict[str, str], ...]:
+        if not comb_class.extra_parameters:
+            return super().extra_parameters(comb_class, children)
+        if children is None:
+            children = self.decomposition_function(comb_class)
+            if children is None:
+                raise StrategyDoesNotApply("Strategy does not apply")
+        child = children[0]
+        mapped_assumptions = tuple(
+            assump.__class__(
+                tuple(
+                    self.forward_map(comb_class, gc, children)[0]
+                    for gc in assump.gcs
+                    if all(cell in self.forward_cell_map(comb_class) for cell in gc.pos)
+                )
+            ).avoiding(child.obstructions)
+            for assump in comb_class.assumptions
+        )
+        return (
+            {
+                comb_class.get_assumption_parameter(
+                    assumption
+                ): child.get_assumption_parameter(mapped_assumption)
+                for assumption, mapped_assumption in zip(
+                    comb_class.assumptions, mapped_assumptions
+                )
+                if mapped_assumption.gcs
+            },
+        )
+
+    @staticmethod
+    def row_col_sep_algorithm(tiling: Tiling) -> RowColSeparation:
+        """Return the algorithm class using tiling."""
+        return RowColSeparation(tiling)
+
+    def formal_step(self) -> str:
+        """Return formal step."""
+        return "row and column separation"
+
+    def backward_map(
+        self,
+        comb_class: Tiling,
+        objs: Tuple[Optional[GriddedChord], ...],
+        children: Optional[Tuple[Tiling, ...]] = None,
+    ) -> Iterator[GriddedChord]:
+        """This method will enable us to generate objects, and sample."""
+        if children is None:
+            children = self.decomposition_function(comb_class)
+        gc = objs[0]
+        assert gc is not None
+        backmap = self.backward_cell_map(comb_class)
+        yield gc.apply_map(backmap.__getitem__)
+
+    def forward_map(
+        self,
+        comb_class: Tiling,
+        obj: GriddedChord,
+        children: Optional[Tuple[Tiling, ...]] = None,
+    ) -> Tuple[GriddedChord]:
+        """This function will enable us to have a quick membership test."""
+        if children is None:
+            children = self.decomposition_function(comb_class)
+        forwardmap = self.forward_cell_map(comb_class)
+        gc = obj.apply_map(forwardmap.__getitem__)
+        return (gc,)
+
+    def __str__(self) -> str:
+        return "row and column separation"
+
+    def __repr__(self) -> str:
+        return "RowColumnSeparationStrategy()"
+
+    @classmethod
+    def from_dict(cls, d) -> "RowColumnSeparationStrategy":
+        return cls()
