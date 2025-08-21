2b3a4eaf4f1f5e0fc7fcae2ce1af53cec023a0cb
Stephanie <sgpenner@uwaterloo.ca>
2025-08-21 15:04:43 -0400

    Fixed strategies except req pl to work with Expand

--- sandbox.py
+++ sandbox.py
@@ -1,8 +1,21 @@
 from chords import Chord, GriddedChord
+from tiling import Tiling
+from algorithms.expansion import Expansion
 
-list_of_chords = Chord.of_length(4)
+'''all_simplify = Tiling(obstructions= (GriddedChord(Chord((0, 0)), ((0, 0), (0, 0))),
+                                     GriddedChord(Chord((0, 0)), ((0, 0), (1, 0))),
+                                     GriddedChord(Chord((0, 0)), ((0, 0), (2, 0))),
+                                     GriddedChord(Chord((0, 0)), ((0, 0), (3, 0))),
+                                     GriddedChord(Chord((0, 0, 1, 1)), ((0, 0), (0, 0), (0, 0), (0, 0))),
+                                     GriddedChord(Chord((0, 0)), ((0, 0), (0, 0))),
+                                     GriddedChord(Chord((0, 1)), ((0, 0), (0, 0))),
+                                     ),
+                     requirements=(#(GriddedChord(Chord((0, 1, 2)), ((1, 1), (1, 1), (2, 2))),),
+                                   (GriddedChord(Chord((0, 1)), ((1, 1), (1, 1))),), # is implied by req 3
+                                   (GriddedChord(Chord((0, 1, 0, 1)), ((1, 1),) * 4),),
+                                   (GriddedChord(Chord((0, 0)), ((1, 0), (2, 0))), GriddedChord(Chord((0, 1, 1, 0)), ((1, 0), (1, 0), (2, 0), (2, 0)))),
+                                   ),)
 
-for chord in list_of_chords: 
-       
-    print(chord)
-
+print(all_simplify.requirements)
+print(all_simplify.obstructions)'''
+print(bool(Chord()))
